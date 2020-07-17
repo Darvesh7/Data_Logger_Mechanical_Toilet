b1d53b0bbe6a03c752299976c63d57d079d6881c
Darvesh7 <47318618+Darvesh7@users.noreply.github.com>
2020-07-17 20:40:57 +0100

    implemented rpm + reverse speed

--- Data_Logger/main.cpp
+++ Data_Logger/main.cpp
@@ -1,75 +1,76 @@
 #include "mbed.h"
-#include "FATFileSystem.h"
-#include "SDBlockDevice.h"
-#include "ds3231.h"
+#include "PinDetect.h"
 #include "RotaryEncoder.h"
-
-#include <stdio.h>
-#include <errno.h>
 #include <string>
-using std::string;
+
+//////////PINS//////////////
+
+PinDetect plunger_switch (PA_0);
+PinDetect encoder_switch (PA_1);
+RotaryEncoder encoder(PC_1, PC_0);
 
 
-#include "platform/mbed_retarget.h"
-
-
+/////////VARIABLES//////////
 #define distance 0.3
 
+int flushCount = 0;
+int pulses = 0;
+float reverse_speed = 0.0;
 
-InterruptIn switchA (PC_13);
-InterruptIn switchB (PA_1);
-Ds3231 rtc(PB_9, PB_8);
-SDBlockDevice sd(SPI_MOSI, SPI_MISO, SPI_SCK, PA_9);
-FATFileSystem fs("sd", &sd);
-RotaryEncoder encoder (PC_1,PC_0);
+//////////RTOS///////////////
+Timer ReverseSpeedTimer,FlushSpeedTimer;
 
-time_t epoch_time;
-FILE * fd;
-
-
-LowPowerTimer switchA_Timer;
-LowPowerTimeout  flush_end;
-
-
+Semaphore SealingUnitSema(1);
 Semaphore loggerSema(1);
 
+void SystemStates_thread(void const *name);
+void SealingUnit_thread(void const *name); 
+void logger(void const *name);
+
+Thread t1, loggerThread;
 
 
-void start_flush(void);
-void end_flush(void);
+
+////////////////////////////
+
+
+
+void plungerSW(void);
+void encoderSW(void);
 
 bool switchA_Pressed = false;
 
+
+/////////TYPEDEF//////////
+
+
+typedef enum 
+{ 
+    SU_Forward, 
+    SU_Backward,
+    SU_Stop,
+} MA_t;
+
+MA_t SealingUnitAction = SU_Stop;
+
+////////////////////////////
+
+
 Serial pc(USBTX, USBRX);
-
-void init_SD(void)
-{
-    pc.baud(115200);
-
-    sd.init();
-    fs.mount(&sd);
-
-   
-    fd = fopen("/sd/testdata.csv", "r+");
-    if (fd != nullptr)
-    {
-        pc.printf("SD Mounted - Ready");
-
-    }
-    else
-    {
-        fd = fopen("/sd/testdata.csv", "w+");
-        fclose(fd);
-        pc.printf("File Closed\r\n");
-        sd.deinit();
-        fs.unmount();
-        pc.printf("Creating new file - No existing file found\r\n");
-    }
-}
-
 
 void setup(void)
 {
+    
+    pc.baud(115200);
+
+
+    plungerSW();
+    encoderSW();
+
+    t1.start(callback(SealingUnit_thread, (void *)"SealingUnitThread"));
+    
+    loggerThread.start(callback(logger, (void *)"DataLoggerThread"));
+
 
 
 }
@@ -77,91 +78,68 @@ void setup(void)
 
 int main()
 {
-    init_SD();
-    setup(); 
-    switchA.fall(&start_flush);
-    switchA.rise(&end_flush);
-  
+    setup();
 
     while(true)
     {
-        
+     if(switchA_Pressed)
+     {
+        pc.printf("%2.2f\n", reverse_speed);
+        switchA_Pressed = false;
+
+     }   
+
 
     }
     
+ 
 }
-
-
-bool sdopened = false;
 
 void logger(void const *name)
 {
     volatile int currenttime = 0;
-    volatile float voltage = 0.0;
-    volatile float current = 0.0;
+    volatile int reversetime = 0;
     volatile int pulses = 0;
     volatile float rpm = 0.0;
-    float RpmRatioConversion = ((600/32)*(1/149.25));
-    volatile float power = 0.0;
-    volatile float torque = 0.0;
+    float RpmRatioConversion = (600/24);
+    
 
 
     
     while(true)
     {
         
-        epoch_time = rtc.get_epoch();
+       
         if(loggerSema.try_acquire_until(20000))
         {
-            if (!sdopened)
-            {
-                sd.init();
-                fs.mount(&sd);
-                fd = fopen("/sd/testdata.csv", "a");
-                fflush(stdout);
-                fprintf(fd, "%s\n", ctime(&epoch_time));
-                sdopened = (fd != nullptr);
-            }
 
 
+           pulses = encoder.getPulses(); 
+           currenttime = FlushSpeedTimer.read_ms();
+           rpm = (pulses*RpmRatioConversion); 
 
 
             string logline;
 
-          
-            //logline.append(to_string(voltage) + ",");
-            //logline.append(to_string(current) + ",");
-            //logline.append(to_string(power)  + ",");
-           
+            logline.append(to_string(flushCount) + ",");
+            logline.append(to_string(pulses) + ",");
+            logline.append(to_string(currenttime) + ",");
+            logline.append(to_string(rpm)  + "\n");
 
             pc.printf("%s", logline.c_str());
-
-            if (fd != nullptr)
-            {
-                
-                fprintf(fd, " %s%s",",",logline.c_str());
-    
-            }
+      
 
             encoder.reset();
-            switchA_Timer.reset();
+            FlushSpeedTimer.reset();
             ThisThread::sleep_for(100);
         
-            if(motor._MState == MFORWARD)
+            if(SealingUnitAction == SU_Forward)
             {
                 loggerSema.release();
             }
             else
             {
-                if (fd != nullptr)
-                {
-                    fflush(stdout);
-                    fprintf(fd, "\r\n");
-                    fclose(fd);
-                    sd.deinit();
-                    fs.unmount();
-                    sdopened = false;
-                }
+
             }
         }
     }
@@ -170,23 +148,77 @@ void logger(void const *name)
 
 
 
-void start_flush(void)
-{           
-   
-    switchA_Timer.start();
-    loggerSema.release();
-    flush_end.attach(&end_flush, 4.0); //timeout - duration of flush    
+void Forward_Button_Pressed(void)
+{
+    if(SealingUnitAction == SU_Stop)
+    {
+        SealingUnitAction = SU_Forward;
+        FlushSpeedTimer.start();
+        SealingUnitSema.release();
+        loggerSema.release();
+        flushCount = flushCount +1;  
+    }
 }
 
-void end_flush(void)
-{        
-    switchA_Timer.stop(); 
-       
-    flush_end.attach(&end_flush, 0.1); //timeout - duration of flush
+
+void Reverse_Button_Released(void)
+{    
+    ReverseSpeedTimer.start();
+    
 }
-   
+
+
+void Forward_Button_Released(void)
+{
+    if(SealingUnitAction == SU_Forward)
+    {
+        SealingUnitAction = SU_Stop;
+        reverse_speed =  (distance / (ReverseSpeedTimer.read_ms()/1000));
+        FlushSpeedTimer.stop();
+        ReverseSpeedTimer.reset();
+        switchA_Pressed = true;
+    }
+}
 
 
 
+void encoderSW(void)
+{
+    encoder_switch.mode(PullUp);
+    encoder_switch.setAssertValue(0);
+    encoder_switch.attach_asserted(&Forward_Button_Pressed);
+    encoder_switch.attach_deasserted(&Forward_Button_Released);
+    encoder_switch.setSampleFrequency();
+
+}
+
+void plungerSW(void)
+{
+    plunger_switch.mode(PullUp);
+    plunger_switch.setAssertValue(0);
+    plunger_switch.attach_deasserted(&Reverse_Button_Released);
+    plunger_switch.setSampleFrequency();
+}
 
 
+
+void SealingUnit_thread(void const *name) 
+{
+    while (true) 
+    {
+        SealingUnitSema.acquire();
+        switch(SealingUnitAction)
+        {
+            case SU_Forward:   
+            pc.printf("forward");           
+                
+            break;
+            case SU_Backward:
+               
+            break;
+            case SU_Stop:
+               
+            break;
+        }
+    }
+}
